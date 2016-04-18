bc6251cdc778a2bd03e8f228ae86e2e5462daea1
alcapriles <alcp.capriles@gmail.com>
2016-04-18 19:47:36 -0300

    Adicionei a versão inicial da classe jogo.

---
+++ jogo.py
@@ -0,0 +1,13 @@
+class Jogo:
+	def __init__(self):
+
+	def recebe_jogada(linha,coluna):
+	#recebe e registra a jogada 
+	#alterna os jogadores
+
+	def verifica_ganhador():
+	#retorna 0 em caso de empate
+	#retorna1 se x for o vencedor
+
+	def limpa_jogadas():
+	#reinicia o jogo mantendo a vez do jogador
